d8df2d1f3bbdbd6fffb8e01ec5d8ab6d49244d25
Purvinigam007 <purvinigam662@gmail.com>
2025-08-24 12:01:50 +0530

    Add files via upload

---
+++ program 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+ printf("My Introduction ----\n");
+    printf("Name - Purvi Nigam\n");
+    printf("Age - 18\n");
+    printf("College - Chandigarh University Lucknow\n");
+    printf("Course - Btech\n");
+    printf("Branch - Cse AI & ML\n");
+    printf("Thankyou....\n");
+    return 0;
+}
