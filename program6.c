fb4345fecfcb55c9c726884d025fd59083f5da00
Purvinigam007 <purvinigam662@gmail.com>
2025-08-28 21:20:40 +0530

    Add files via upload

---
+++ program6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int year;
+printf("Enter year:");
+scanf("%d",&year);
+if(year%4==0||year%400==0)
+printf("%d is leap year",year);
+else("%d is not a leap year",year);
+return0;
+}
