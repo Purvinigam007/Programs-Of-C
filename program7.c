ce4891bf8679459fc351a2d8b7a9f04d544c854c
Purvinigam007 <purvinigam662@gmail.com>
2025-08-28 21:22:43 +0530

    Add files via upload

---
+++ program7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+int day;
+printf("Enter the day number\n");
+scanf("%d",&day);
+switch(day){
+    case 1:
+printf(" day is Monday\n");
+
+   case 2:
+    printf("Day is Tuesday\n");
+    break;
+case 3:
+printf("Day is Wednesday\n");}
+
+return 0;
+}
