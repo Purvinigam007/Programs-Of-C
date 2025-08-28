e5086d667c37ef147f0c4c28cae3941cf4d4cde1
Purvinigam007 <purvinigam662@gmail.com>
2025-08-28 21:24:36 +0530

    Add files via upload

---
+++ program8.c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+int number;
+printf("Enter number");
+scanf("%d",&number);
+if(number>0){
+printf("It is positive");}
+
+else if(number<0){
+printf("it is negative");}
+
+else{
+printf("It is zero");
+}
+return 0;
+
+}
